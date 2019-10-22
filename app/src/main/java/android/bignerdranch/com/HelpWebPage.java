5aaa8e37effd621c331445ad7150a2cb73b7c342
ryanh <ryanhansen_17@hotmail.com>
2019-10-22 12:51:14 +1000

    list ui and item ui

---
+++ app/src/main/java/android/bignerdranch/com/HelpWebPage.java
@@ -0,0 +1,15 @@
+package android.bignerdranch.com;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HelpWebPage extends AppCompatActivity {
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState){
+
+    }
+}
